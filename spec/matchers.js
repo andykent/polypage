908aab788f0589307c417da83500db247641760d
Andy Kent <andrew.d.kent@gmail.com>
2009-03-07 14:54:40 +0000

    started to add some long overdue specs

---
+++ spec/matchers.js
@@ -0,0 +1,10 @@
+Screw.Matchers["be_visible"] = {
+	match: function(expected, actual) {
+		if (!(actual instanceof jQuery)) actual = jQuery(actual)
+		if(actual.length==0) throw "Unable to determine visibility as no elements matched "+actual.toString();
+		return actual.is(':visible');
+	},
+	failure_message: function(expected, actual, not) {
+		return 'expected ' + $.print(actual) + (not ? ' not' : '') + ' to be visible';
+	}
+}
---
+++ spec/public-api-spec.js
@@ -0,0 +1,50 @@
+Screw.Unit(function() {
+	describe("PolyPage Public API", function() {
+		describe("Using the toggle box", function() {
+			it("toggles elements", function() {
+				$.polypage.setState('logged_in', false);
+				expect('.pp_logged_in').to_not(be_visible);
+				$("#pp_state_switch_logged_in").trigger('click');
+				expect('.pp_logged_in').to(be_visible);
+				$("#pp_state_switch_logged_in").trigger('click');
+				expect('.pp_logged_in').to_not(be_visible);
+			});
+		});
+		
+		describe("$.polypage.setState()", function() {
+			it("shows and hides matching elements", function() {
+				$.polypage.setState('logged_in', false);
+				expect('.pp_logged_in').to_not(be_visible);
+				$.polypage.setState('logged_in', true);
+				expect('.pp_logged_in').to(be_visible);
+			});
+			
+			it("shows and hides inverted matching elements", function() {
+				$.polypage.setState('logged_in', false);
+				expect('.pp_not_logged_in').to(be_visible);
+				$.polypage.setState('logged_in', true);
+				expect('.pp_not_logged_in').to_not(be_visible);
+			});
+
+			it("shows and hides AND'ed matching elements", function() {
+				$.polypage.setState('logged_in', false);
+				$.polypage.setState('admin', false);
+				expect('.pp_logged_in_and_admin').to_not(be_visible);
+				$.polypage.setState('logged_in', true);
+				expect('.pp_logged_in_and_admin').to_not(be_visible);
+				$.polypage.setState('admin', true);
+				expect('.pp_logged_in_and_admin').to(be_visible);
+			});
+			
+			it("shows and hides OR'ed matching elements", function() {
+				$.polypage.setState('logged_in', false);
+				$.polypage.setState('admin', false);
+				expect('.pp_logged_in_or_admin').to_not(be_visible);
+				$.polypage.setState('logged_in', true);
+				expect('.pp_logged_in_or_admin').to(be_visible);
+				$.polypage.setState('admin', true);
+				expect('.pp_logged_in_or_admin').to(be_visible);
+			});
+		});
+	});
+});
