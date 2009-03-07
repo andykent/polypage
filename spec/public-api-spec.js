83a6c925a9a79fcf3a431346501cc4bb56f718c3
Andy Kent <andrew.d.kent@gmail.com>
2009-03-07 22:42:25 +0000

    public state change event triggers API

--- spec/public-api-spec.js
+++ spec/public-api-spec.js
@@ -1,50 +1,59 @@
 Screw.Unit(function() {
-	describe("PolyPage Public API", function() {
-		describe("Using the toggle box", function() {
-			it("toggles elements", function() {
-				$.polypage.setState('logged_in', false);
-				expect('.pp_logged_in').to(be_hidden);
-				$("#pp_state_switch_logged_in").trigger('click');
-				expect('.pp_logged_in').to(be_visible);
-				$("#pp_state_switch_logged_in").trigger('click');
-				expect('.pp_logged_in').to(be_hidden);
-			});
-		});
-		
-		describe("$.polypage.setState()", function() {
-			it("shows and hides matching elements", function() {
-				$.polypage.setState('logged_in', false);
-				expect('.pp_logged_in').to(be_hidden);
-				$.polypage.setState('logged_in', true);
-				expect('.pp_logged_in').to(be_visible);
-			});
-			
-			it("shows and hides inverted matching elements", function() {
-				$.polypage.setState('logged_in', false);
-				expect('.pp_not_logged_in').to(be_visible);
-				$.polypage.setState('logged_in', true);
-				expect('.pp_not_logged_in').to(be_hidden);
-			});
+  describe("PolyPage Public Event API", function() {
+    describe("triggering pp_setState events", function() {
+      it("shows and hides matching elements", function() {
+        $('#dom').trigger('pp_setState', { logged_in:false });
+        expect('.pp_logged_in').to(be_hidden);
+        $('#dom').trigger('pp_setState', { logged_in:true });
+        expect('.pp_logged_in').to(be_visible);
+      });
 
-			it("shows and hides AND'ed matching elements", function() {
-				$.polypage.setState('logged_in', false);
-				$.polypage.setState('admin', false);
-				expect('.pp_logged_in_and_admin').to(be_hidden);
-				$.polypage.setState('logged_in', true);
-				expect('.pp_logged_in_and_admin').to(be_hidden);
-				$.polypage.setState('admin', true);
-				expect('.pp_logged_in_and_admin').to(be_visible);
-			});
-			
-			it("shows and hides OR'ed matching elements", function() {
-				$.polypage.setState('logged_in', false);
-				$.polypage.setState('admin', false);
-				expect('.pp_logged_in_or_admin').to(be_hidden);
-				$.polypage.setState('logged_in', true);
-				expect('.pp_logged_in_or_admin').to(be_visible);
-				$.polypage.setState('admin', true);
-				expect('.pp_logged_in_or_admin').to(be_visible);
-			});
-		});
-	});
+      it("shows and hides inverted matching elements", function() {
+        $('#dom').trigger('pp_setState', { logged_in:false });
+        expect('.pp_not_logged_in').to(be_visible);
+        $('#dom').trigger('pp_setState', { logged_in:true });
+        expect('.pp_not_logged_in').to(be_hidden);
+      });
+
+      it("shows and hides AND'ed matching elements", function() {
+        $('#dom').trigger('pp_setStates', { logged_in:false, admin:false });
+        expect('.pp_logged_in_and_admin').to(be_hidden);
+        $('#dom').trigger('pp_setState', { logged_in:true });
+        expect('.pp_logged_in_and_admin').to(be_hidden);
+        $('#dom').trigger('pp_setState', { admin:true });
+        expect('.pp_logged_in_and_admin').to(be_visible);
+      });
+
+      it("shows and hides OR'ed matching elements", function() {
+        $('#dom').trigger('pp_setStates', { logged_in:false, admin:false });
+        expect('.pp_logged_in_or_admin').to(be_hidden);
+        $('#dom').trigger('pp_setState', { logged_in:true });
+        expect('.pp_logged_in_or_admin').to(be_visible);
+        $('#dom').trigger('pp_setState', { admin:true });
+        expect('.pp_logged_in_or_admin').to(be_visible);
+      });
+    });
+    
+    describe("triggering pp_toggleState events", function() {
+      it("toggles a single state", function() {
+        $('#dom').trigger('pp_setState', { logged_in:false });
+        $('#dom').trigger('pp_toggleState', 'logged_in');
+        expect('.pp_logged_in').to(be_visible);
+        $('#dom').trigger('pp_toggleState', 'logged_in');
+        expect('.pp_logged_in').to(be_hidden);
+      });
+    });
+    
+    describe("triggering pp_toggleStates events", function() {
+      it("toggles multiple states", function() {
+        $('#dom').trigger('pp_setState', { logged_in:false, admin:true });
+        $('#dom').trigger('pp_toggleStates', ['logged_in', 'admin']);
+        expect('.pp_logged_in').to(be_visible);
+        expect('.pp_admin').to(be_hidden);
+        $('#dom').trigger('pp_toggleStates', ['logged_in', 'admin']);
+        expect('.pp_logged_in').to(be_hidden);
+        expect('.pp_admin').to(be_visible);
+      });
+    });
+  });
 });
