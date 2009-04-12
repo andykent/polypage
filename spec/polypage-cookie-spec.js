79272d02d57646b4a187db3c9c9179493689a009
Andy Kent <andrew.d.kent@gmail.com>
2009-04-12 18:27:03 +0100

    prefix cookies correctly

--- spec/polypage-cookie-spec.js
+++ spec/polypage-cookie-spec.js
@@ -1,5 +1,22 @@
 Screw.Unit(function() {
   describe("PolyPage Cookie", function() {
+    it("stores state in a cookie when switched on", function() {
+      $('#dom').trigger('pp_setState', { logged_in:false });
+      $('#dom').trigger('pp_setState', { logged_in:true });
+      expect($.cookie('pp_logged_in')).to(equal, 'yes');
+    });
     
+    it("stores forgets state in a cookie when switched off", function() {
+      $('#dom').trigger('pp_setState', { logged_in:true });
+      $('#dom').trigger('pp_setState', { logged_in:false });
+      expect($.cookie('pp_logged_in')).to(equal, null);
+    });
+    
+    it("stores state in a cookie when toggled", function() {
+      $('#dom').trigger('pp_toggleState', 'logged_in');
+      expect($.cookie('pp_logged_in')).to(equal, 'yes');
+      $('#dom').trigger('pp_toggleState', 'logged_in');
+      expect($.cookie('pp_logged_in')).to(equal, null);
+    });
   });
 });
